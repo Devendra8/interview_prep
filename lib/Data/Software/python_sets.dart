badc906cc34203e49f4581acefca6277e45b8eec
Devendra Patil <81964889+Devendra8@users.noreply.github.com>
2022-03-01 11:46:43 +0530

    added python questions

--- lib/Data/Software/python_sets.dart
+++ lib/Data/Software/python_sets.dart
@@ -4,37 +4,86 @@ import 'package:interview_prep/Model/Software/python_model.dart';
 final pythonSets = [
   python(
     number: "Q.1",
-    title: "What Is Face Yoga?",
+    title: "What type of language is python? Programming or scripting?",
     paragraph:
-        "Face yoga is similar to yoga for the body in that it consists of exercises and stretches that target the muscles in the face.",
+        "Python is capable of scripting, but in general sense, it is considered as a general-purpose programming language.",
     color: Color(0xFFb5651d),
+    source:
+        "https://www.edureka.co/blog/interview-questions/python-interview-questions/",
   ),
   python(
     number: "Q.2",
-    title: "What Are the Benefits of Face Yoga?",
+    title: "Python an interpreted language. Explain.",
     paragraph:
-        "Fumiko Takatsu, founder of the Face Yoga Method, claims a few minutes per day of stretching will tone and lift the entire body, but the scientific evidence is mixed.",
+        "An interpreted language is any programming language which is not in machine-level code before runtime. Therefore, Python is an interpreted language",
     color: Color(0xFF18A558),
+    source:
+        "https://www.edureka.co/blog/interview-questions/python-interview-questions/",
   ),
   python(
     number: "Q.3",
-    title: "Which specific areas can doing face yoga exercises help?",
+    title: "What is pep 8?",
     paragraph:
-        "Face yoga can be used to target any part of your face, depending on your concerns and goals. If you have tightness in your forehead, brows, or jaw, design your routine around these places. Around the forehead, eyes, and mouth, wrinkles are common. ",
+        "PEP stands for Python Enhancement Proposal. It is a set of rules that specify how to format Python code for maximum readability.",
     color: Color(0xFFb5651d),
+    source:
+        "https://www.edureka.co/blog/interview-questions/python-interview-questions/",
   ),
   python(
     number: "Q.4",
-    title: "Which types of yoga might be better for your face?",
+    title: "What are decorators in Python?",
     paragraph:
-        "The slower forms of yoga that involve holding poses for extended periods may give you more chances to bring this awareness to your facial muscles. Some examples are Hatha, Yin, and restorative yoga. During meditation, pranayama, or yoga nidra, you can also relax your face muscles.",
+        "Decorators are used to add some design patterns to a function without changing its structure.\n\n Decorators generally are defined before the function they are enhancing. To apply a decorator we first define the decorator function.\n \n Then we write the function it is applied to and simply add the decorator function above the function it has to be applied to. For this, we use the @ symbol before the decorator.",
     color: Color(0xFFe1ad01),
+    source:
+        "https://www.edureka.co/blog/interview-questions/python-interview-questions/",
   ),
   python(
     number: "Q.5",
-    title: "How to do face yoga exercises",
+    title: "What is the difference between .py and .pyc files?",
     paragraph:
-        "In order to perform these exercises effectively, you should do them in front of a mirror at home, since they may appear a bit silly when performed in public. When performing the exercises, keep the rest of your face, neck, and shoulders relaxed. Keeping your eyes on your reflection will help you to avoid pressing or massaging so forcefully that you fold your skin in on itself.",
+        "The .py files are the python source code files. While the .pyc files contain the bytecode of the python files. .pyc files are created when the code is imported from some other source.\n\n The interpreter converts the source .py files to .pyc files which helps by saving time.",
     color: Color(0xFF18A558),
+    source:
+        "https://www.edureka.co/blog/interview-questions/python-interview-questions/",
+  ),
+  python(
+    number: "Q.6",
+    title: "What is an Interpreted language?",
+    paragraph:
+        "An Interpreted language executes its statements line by line. Languages such as Python, Javascript, R, PHP, and Ruby are prime examples of Interpreted languages.\n\n Programs written in an interpreted language runs directly from the source code, with no intermediary compilation step.",
+    color: Color(0xFF18A558),
+    source: "https://www.interviewbit.com/python-interview-questions/",
+  ),
+  python(
+    number: "Q.7",
+    title: "What is Scope in Python?",
+    paragraph:
+        "Every object in Python functions within a scope. A scope is a block of code where an object in Python remains relevant.\n\n Namespaces uniquely identify all the objects inside a program. However, these namespaces also have a scope defined for them where you could use their objects without any prefix.\n\n A few examples of scope created during code execution in Python are as follows:\n\n 1. A local scope refers to the local objects available in the current function.\n\n 2.A global scope refers to the objects available throughout the code execution since their inception. \n\n 3.A module-level scope refers to the global objects of the current module accessible in the program. \n\n 4.  An outermost scope refers to all the built-in names callable in the program. The objects in this scope are searched last to find the name referenced.  ",
+    color: Color(0xFF18A558),
+    source: "https://www.interviewbit.com/python-interview-questions/",
+  ),
+  python(
+    number: "Q.8",
+    title: "What is pass in Python?",
+    paragraph:
+        "The pass keyword represents a null operation in Python. It is generally used for the purpose of filling up empty blocks of code which may execute during runtime but has yet to be written. Without the pass statement in the following code, we may run into some errors during code execution.",
+    color: Color(0xFF18A558),
+    source: "https://www.interviewbit.com/python-interview-questions/",
+  ),
+  python(
+    number: "Q.9",
+    title: "What is docstring in Python?",
+    paragraph:
+        "1. Documentation string or docstring is a multiline string used to document a specific code segment. \n\n 2. The docstring should describe what the function or method does. ",
+    color: Color(0xFF18A558),
+    source: "https://www.interviewbit.com/python-interview-questions/",
+  ),
+  python(
+    number: "Q.10",
+    title: "Explain how can you make a Python Script executable on Unix?",
+    paragraph: "Script file must begin with #!/usr/bin/env python",
+    color: Color(0xFF18A558),
+    source: "https://www.interviewbit.com/python-interview-questions/",
   ),
 ];
